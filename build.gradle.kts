dd4e09086faa54346ca9de085d8c3b11351a2a0c
TheRealJan <jan.m.tennert@gmail.com>
2023-03-03 16:39:46 +0100

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,17 @@ plugins {
     id("com.android.library")
     kotlin("plugin.serialization") version Versions.KOTLIN
     `maven-publish`
+    id("org.jetbrains.dokka") version Versions.DOKKA
+    id("io.codearte.nexus-staging") version Versions.NEXUS_STAGING
+    signing
+}
+
+nexusStaging {
+    stagingProfileId = Publishing.PROFILE_ID
+    stagingRepositoryId.set(Publishing.REPOSITORY_ID)
+    username = Publishing.SONATYPE_USERNAME
+    password = Publishing.SONATYPE_PASSWORD
+    serverUrl = "https://s01.oss.sonatype.org/service/local/"
 }
 
 group = "io.github.jan-tennert"
@@ -56,5 +67,103 @@ android {
     }
     lint {
         abortOnError = false
+    }
+}
+
+val dependsOnTasks = mutableListOf<String>()
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    dependsOnTasks.add(this.name.replace("publish", "sign").replaceAfter("Publication", ""))
+    dependsOn(dependsOnTasks)
+}
+
+signing {
+    val signingKey = providers
+        .environmentVariable("GPG_SIGNING_KEY")
+        .orElse(File(System.getenv("GPG_PATH") ?: "").let {
+            try {
+                it.readText()
+            } catch(_: Exception) {
+                ""
+            }
+        })
+        .forUseAtConfigurationTime()
+    val signingPassphrase = providers
+        .environmentVariable("GPG_SIGNING_PASSPHRASE")
+        .forUseAtConfigurationTime()
+
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions
+            .getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
+}
+publishing {
+    repositories {
+        maven {
+            name = "Oss"
+            setUrl {
+                "https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/${Publishing.REPOSITORY_ID}"
+            }
+            credentials {
+                username = Publishing.SONATYPE_USERNAME
+                password = Publishing.SONATYPE_PASSWORD
+            }
+        }
+        maven {
+            name = "Snapshot"
+            setUrl { "https://s01.oss.sonatype.org/content/repositories/snapshots/" }
+            credentials {
+                username = Publishing.SONATYPE_USERNAME
+                password = Publishing.SONATYPE_PASSWORD
+            }
+        }
+    }
+//val dokkaOutputDir = "H:/Programming/Other/DiscordKMDocs"
+    val dokkaOutputDir = "$buildDir/dokka/openai-kt}"
+
+    tasks.dokkaHtml {
+        outputDirectory.set(file(dokkaOutputDir))
+    }
+
+    val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+        delete(dokkaOutputDir)
+    }
+
+    val javadocJar = tasks.register<Jar>("javadocJar") {
+        dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+        archiveClassifier.set("javadoc")
+        from(dokkaOutputDir)
+    }
+
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("openai-kt")
+                description.set("A Kotlin Multiplatform SDK for OpenAI APIs")
+                url.set("https://github.com/jan-tenenrt/openai-kt")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://mit-license.org/")
+                    }
+                }
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/jan-tenenrt/openai-kt/issues")
+                }
+                scm {
+                    connection.set("https://github.com/jan-tenenrt/openai-kt.git")
+                    url.set("https://github.com/jan-tenenrt/openai-kt")
+                }
+                developers {
+                    developer {
+                        name.set("TheRealJanGER")
+                        email.set("jan.m.tennert@gmail.com")
+                    }
+                }
+            }
+        }
     }
 }
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,10 @@
 object Versions {
 
     const val KOTLIN = "1.8.10"
-    const val KTOR = "2.2.3"
+    const val KTOR = "2.2.4"
     const val COROUTINES = "1.6.4"
     const val DOKKA = "1.7.20"
     const val NAPIER = "2.6.1"
+    const val NEXUS_STAGING = "0.30.0"
 
 }
